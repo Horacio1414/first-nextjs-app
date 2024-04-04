ad9c4c115d333898fd1db6c1e9848f4ab5e6d71b
Horacio Demaria Hontan <horacio.demaria@globant.com>
2024-04-04 12:12:10 -0300

    [first next/vercel app

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nesciunt
+        consequuntur minima quos, labore at corrupti quisquam ad qui magni?
+        Maiores magni modi nobis a distinctio, veritatis deserunt. Voluptatum
+        beatae quaerat natus laborum ullam animi cum. Ducimus delectus magnam
+        veritatis facere. Vitae aspernatur veniam porro expedita, totam
+        voluptatibus vel odit provident error esse natus inventore at incidunt
+        aperiam reiciendis soluta debitis iure velit, amet ea deserunt.
+        Perferendis voluptatum consectetur odit, laudantium odio aliquam quae
+        culpa in sint, quod doloribus fugiat quam illum praesentium sequi, ab
+        dolor voluptate temporibus laborum. Nostrum placeat dolor, voluptates
+        eos odit sed nemo fuga natus voluptas.
+      </p>
+    </div>
+  );
+}
+
+export default page;
---
+++ app/services/page.jsx
@@ -0,0 +1,17 @@
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate
+        dignissimos porro provident consectetur exercitationem dolore, eos
+        molestiae soluta sapiente placeat libero minus mollitia, neque illum
+        quisquam rerum quaerat, obcaecati odit asperiores veritatis. Quibusdam
+        mollitia illum aut similique eligendi incidunt et commodi dolorum
+        suscipit a. Sit rerum voluptatibus error iure!
+      </p>
+    </div>
+  );
+}
+
+export default ServicePage;
